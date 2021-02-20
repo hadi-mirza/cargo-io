65e1a5cb53d4ca94b477a0f8a3ada9cf607f0e25
hadi-mirza <hadimirza1@gmail.com>
2021-02-19 21:08:13 -0500

    add login and sign up page. add react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import Login from './components/auth/Login/Login'
 import SignUp from './components/auth/SignUp/SignUp'
+import { Route } from 'react-router-dom';
+import HomePage from './pages/Home/HomePage.jsx'
 
 class App extends React.Component {
   constructor() {
@@ -19,8 +20,22 @@ class App extends React.Component {
           <code>cargo.io</code>
         </p>
       </header>
-      <Login />
-      <SignUp />
+      {/* <Login />
+      <SignUp /> */}
+      <HomePage />
+      <Route exact path='/signup' render={({ history }) => 
+            <SignUp
+              history={history}
+              handleSignup={this.handleSignup}
+              
+            />
+          }/>
+          <Route exact path='/login' render={({history}) => 
+            <Login
+              history={history}
+              handleLogin={this.handleLogin}
+            />
+          }/>
     </div>
     )
   }
