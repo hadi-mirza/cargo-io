b9d3f5bdcdd089a39475e8daa949675ef782c8a2
hadi-mirza <hadimirza1@gmail.com>
2021-02-26 09:13:16 -0500

    remove leaflet module

--- src/pages/PickupDetail/PickupDetail.jsx
+++ src/pages/PickupDetail/PickupDetail.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import requestService from "../../utils/requestService";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import mapPic from './map1.png'
 
 const position = [43.6532, -79.3832];
@@ -12,19 +11,6 @@ function PickupDetail() {
       <img src={mapPic} />
       <br /><br />
       <h2>Pickup Status: Pending</h2>
-      {/* <div className="mapdiv">
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-      </div> */}
     </div>
   );
 }
